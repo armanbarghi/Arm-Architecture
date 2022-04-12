436f346fc291b9c5dcebfc8b2da3b0dbe8efcb57
neushaj <neushajavidnia@ymail.com>
2022-04-12 10:48:07 +0430

    Add Files

---
+++ lab 1/IF_Stage.v
@@ -0,0 +1,5 @@
+module IF_Stage(input clk, rst, freeze, Branch_taken,
+                input [31:0] BranchAddr,
+                output reg [31:0] PC, Inst);
+
+endmodule
---
+++ lab 1/IF_Stage_Reg.v
@@ -0,0 +1,5 @@
+module IF_Stage_Reg(input clk, rst, freeze, flush,
+                input [31:0] PC,
+                output reg [31:0] PC, Inst);
+
+endmodule
