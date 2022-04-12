2b2ba91f77af1d39ee93fdd2d84ab3b72a1b720c
Arman Barghi <arman.barghi@gmail.com>
2022-04-12 10:59:13 +0430

    add

--- lab 1/Register.v
+++ lab 1/Register.v
@@ -1,1 +1,15 @@
-
+module reg_32b (d_in, sclr, ld, clk, d_out);
+  input [31:0] d_in;
+  input sclr, ld, clk;
+  output [31:0] d_out;
+  reg [31:0] d_out;
+  
+  always @(posedge clk)
+  begin
+    if (sclr==1'b1)
+      d_out = 32'd0;
+    else if (ld)
+      d_out = d_in;
+  end
+  
+endmodule
