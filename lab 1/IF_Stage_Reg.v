ba74c72b54325728c8e8f85dfb77f08aed6e96db
neushaj <neushajavidnia@ymail.com>
2022-04-12 10:56:42 +0430

    add

--- lab 1/IF_Stage_Reg.v
+++ lab 1/IF_Stage_Reg.v
@@ -1,5 +1,6 @@
 module IF_Stage_Reg(input clk, rst, freeze, flush,
-                input [31:0] PC,
+                input [31:0] PC_in, Inst_in,
                 output reg [31:0] PC, Inst);
 
 endmodule
+
