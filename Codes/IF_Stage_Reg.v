cdd2212ab4b2a4527580552e2662ffdccadb0be5
Arman Barghi <arman.barghi@gmail.com>
2022-05-07 18:45:02 +0430

    Beautify IF_Reg

--- Codes/IF_Stage_Reg.v
+++ Codes/IF_Stage_Reg.v
@@ -3,30 +3,27 @@ module IF_Stage_Reg (
     PC_in, Inst_in,
     PC, Inst
 );
-
     input clk, rst, freeze, flush;
     input [31:0] PC_in, Inst_in;
     output reg [31:0] PC, Inst;
     
-    always @(posedge clk, posedge rst)
-        begin
-            if(rst == 1'b1)begin
-                PC <= 32'b0;
-                Inst <= 32'b0;
-            end
-            else if(flush == 1'b1)begin
-                PC <= 32'b0;
-                Inst <= 32'b0;
-            end
-            else if(freeze == 1'b1)begin
-                PC <= PC;
-                Inst <= Inst;
-            end
-            else begin
-                PC <= PC_in;
-                Inst <= Inst_in;
-            end 
+    always @(posedge clk, posedge rst) begin
+        if(rst == 1'b1) begin
+            PC <= 32'b0;
+            Inst <= 32'b0;
         end
+        else if(flush == 1'b1) begin
+            PC <= 32'b0;
+            Inst <= 32'b0;
+        end
+        else if(freeze == 1'b1) begin
+            PC <= PC;
+            Inst <= Inst;
+        end
+        else begin
+            PC <= PC_in;
+            Inst <= Inst_in;
+        end 
+    end
         
 endmodule
-
