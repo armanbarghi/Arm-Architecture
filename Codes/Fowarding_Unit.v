716efda965edd898aa5d5f0245bbed3b2563ded6
neushaj <neushajavidnia@ymail.com>
2022-05-13 03:58:43 +0430

    Final Forwarding unit & Arm work well!

--- Codes/Fowarding_Unit.v
+++ Codes/Fowarding_Unit.v
@@ -12,21 +12,19 @@ module Forwarding_Unit (
     output reg [1:0] sel_src1, sel_src2;
 
     always @(src1, src2, mem_wb_en, wb_wb_en, mem_dest, wb_dest) begin
+        sel_src1 = 2'b00;
+        sel_src2 = 2'b00;
         if (mem_wb_en) begin
             if (src1 == mem_dest)
                 sel_src1 = 2'b01;
             if (src2 == mem_dest)
                 sel_src2 = 2'b01;
         end
-        else if (wb_wb_en) begin
+        if (wb_wb_en) begin
             if (src1 == wb_dest)
                 sel_src1 = 2'b10;
             if (src2 == wb_dest)
                 sel_src2 = 2'b10;
-        end
-        else begin
-            sel_src1 = 2'b00;
-            sel_src2 = 2'b00;
         end
     end
 
