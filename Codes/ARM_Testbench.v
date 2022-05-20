65e35dd0ea3e35161192988f3f9c84b1f924966d
Arman Barghi <arman.barghi@gmail.com>
2022-05-20 10:18:38 +0430

    Change testbench file name

---
+++ Codes/ARM_Testbench.v
@@ -0,0 +1,20 @@
+`timescale 1ps/1ps
+module ARM_Testbench();
+    reg clk, rst, mode;
+
+    ARM_cpu ARM_PROCESSOR(clk, rst, mode);
+
+    initial begin
+        rst = 1'b1;
+        clk = 1'b1;
+        mode = 1'b1;
+        #20 rst = 1'b0;
+        #15000;
+        $stop;
+    end
+
+    always begin
+        #10 clk = ~clk;
+    end
+
+endmodule
